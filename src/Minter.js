5b538c4df2127a8c41d5dbd76c91226747c4fa2d
mesutbunaldi <mesut.bunaldi@softtech.com.tr>
2023-07-27 19:28:14 +0300

    first

---
+++ src/Minter.js
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected, mintNFT } from "./utils/interact.js";
+
+const Minter = (props) => {
+
+  //State variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+ 
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  useEffect(async () => {
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status); 
+    addWalletListener();
+  }, []);
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  const onMintPressed = async () => {
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title">Softtech NFT Minter</h1>
+      <p>
+        Add your asset's link, name, and description, then press "Mint."
+      </p>
+      <form>
+        <h2>Link to asset: </h2>
+        <input
+          type="text"
+          placeholder="https://softtech.com/logo.png"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>Name: </h2>
+        <input
+          type="text"
+          placeholder="My first NFT!"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <h2>Description: </h2>
+        <input
+          type="text"
+          placeholder="Text Description of NFT "
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        Mint NFT
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
